af5df1b83b6c2314ca06839b70c6fc760984d2e0
Alexander SarGon <lexsargon@gmail.com>
2021-02-10 19:46:32 +0000

    Added Contact Form Mailer File

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'lexsargon@gmail.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
